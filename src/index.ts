e3e37410a70357cacf8376a0668c3680f21a8013
Brayan Torres <torresacunabrayan460@gmail.com>
2023-01-19 12:08:01 -0500

    Create models and relations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
 
 import { PrismaClient } from "@prisma/client";
 
@@ -10,10 +11,35 @@ const app: Express = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
   res.send("Usando Typescript y Express");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Servidor en el puerto ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor en el puerto ${PORT}`);
+});
+
+//Usuarios
+
+app.post("/api/v1/users", async (req: Request, res: Response) => {
+  const { name, email, password} = req.body;
+  const last_session = new Date();
+  const update_at = new Date();
+  const date_born = new Date()
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      last_session,
+      update_at,
+      date_born
+    },
+  });
+  res.status(201).json(user);
+});
