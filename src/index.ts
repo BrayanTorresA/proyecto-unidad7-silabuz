8e78c0e04bd891b3785bbd4e22f6e28601b37703
Brayan Torres <torresacunabrayan460@gmail.com>
2023-01-19 22:52:18 -0500

    Create endpoint for login users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import cors from "cors";
+import * as jwt from "jsonwebtoken";
 
 import { PrismaClient } from "@prisma/client";
 
@@ -10,6 +12,7 @@ const prisma = new PrismaClient();
 const app: Express = express();
 
 const PORT = process.env.PORT;
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 
@@ -24,10 +27,10 @@ app.listen(PORT, () => {
 //Usuarios
 
 app.post("/api/v1/users", async (req: Request, res: Response) => {
-  const { name, email, password} = req.body;
+  const { name, email, password } = req.body;
   const last_session = new Date();
   const update_at = new Date();
-  const date_born = new Date()
+  const date_born = new Date();
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -38,8 +41,68 @@ app.post("/api/v1/users", async (req: Request, res: Response) => {
       password: hashedPassword,
       last_session,
       update_at,
-      date_born
+      date_born,
     },
   });
   res.status(201).json(user);
+});
+
+//Login usuarios
+
+app.post("/api/v1/users/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email" });
+  }
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ auth: false, message: "Incorrect password" });
+  }
+
+  const token = jwt.sign(user, process.env.TOKEN_SECRET!, {
+    expiresIn: 60 * 60 * 24,
+  });
+  res.json({ auth: true, token });
+});
+
+//Crear Canción
+
+app.post("/api/v1/songs", async (req: Request, res: Response) => {
+  const { name, artist, album, year, genre, duration, is_public } = req.body;
+  const song = await prisma.song.create({
+    data: {
+      name,
+      artist,
+      album,
+      year,
+      genre,
+      duration,
+      is_public,
+    },
+  });
+
+  res.status(201).json(song);
+});
+
+//Listar Canciones
+app.get("/api/v1/songs", async (_req: Request, res: Response) => {
+  const results = await prisma.song.findMany({
+    where: { is_public: true },
+  });
+  res.json(results);
+});
+
+//Listar cancion por id
+app.get("/api/v1/songs/:id", async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id);
+  const result = await prisma.song.findUnique({
+    where: {
+      id,
+    },
+  });
+  res.json(result);
 });
