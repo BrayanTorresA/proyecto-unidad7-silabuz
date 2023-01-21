1bc42cf85709ac5cb546a6be51d55729c6bf4ec3
Brayan Torres <torresacunabrayan460@gmail.com>
2023-01-20 21:52:16 -0500

    configure models

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { application, Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import cors from "cors";
@@ -29,82 +29,190 @@ app.listen(PORT, () => {
 //Usuarios
 
 app.post("/api/v1/users", async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-  const last_session = new Date();
-  const update_at = new Date();
-  const date_born = new Date();
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  try {
+    const { name, email, password, date_born } = req.body;
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-      last_session,
-      update_at,
-      date_born,
-    },
-  });
-  res.status(201).json(user);
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        date_born: new Date(date_born),
+      },
+    });
+    res.status(201).json({ message: "user created successfully", info: user });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
 //Login usuarios
 
 app.post("/api/v1/users/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-  if (!user) {
-    return res.status(401).json({ message: "Invalid email" });
-  }
-  const passwordIsValid = await bcrypt.compare(password, user.password);
-  if (!passwordIsValid) {
-    return res.status(401).json({ auth: false, message: "Incorrect password" });
-  }
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email" });
+    }
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      return res
+        .status(401)
+        .json({ auth: false, message: "Incorrect password" });
+    }
 
-  const token = jwt.sign(user, process.env.TOKEN_SECRET!, {
-    expiresIn: 60 * 60 * 24,
-  });
-  res.json({ auth: true, token });
+    const token = jwt.sign(user, process.env.TOKEN_SECRET!, {
+      expiresIn: 60 * 60 * 24,
+    });
+    res.json({ auth: true, token });
+  } catch (error) {
+    res.status(500).json({ auth: false, message: error });
+  }
 });
 
 //Crear Canción
 
 app.post("/api/v1/songs", async (req: Request, res: Response) => {
-  const { name, artist, album, year, genre, duration, is_public } = req.body;
-  const song = await prisma.song.create({
-    data: {
-      name,
-      artist,
-      album,
-      year,
-      genre,
-      duration,
-      is_public,
-    },
-  });
+  try {
+    const { name, artist, album, year, genre, duration, is_public } = req.body;
+    const song = await prisma.song.create({
+      data: {
+        name,
+        artist,
+        album,
+        year,
+        genre,
+        duration,
+        is_public,
+      },
+    });
 
-  res.status(201).json(song);
+    res.status(201).json({ message: "created successfully", info: song });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
-//Listar Canciones
+//Listar Canciones publicas
 app.get("/api/v1/songs", async (_req: Request, res: Response) => {
-  const results = await prisma.song.findMany({
-    where: { is_public: true },
-  });
-  res.json(results);
+  try {
+    const publicSongs = await prisma.song.findMany({
+      where: { is_public: true },
+    });
+    res.json(publicSongs);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
+
+//listar todas las canciones(incluida las privadas)
+app.get(
+  "/api/v1/songs/all",
+  verifyToken,
+  async (_req: Request, res: Response) => {
+    try {
+      const songs = await prisma.song.findMany();
+      res.json(songs);
+    } catch (error) {}
+  }
+);
 
 //Listar cancion por id
-app.get("/api/v1/songs/:id",verifyToken, async (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id);
-  const result = await prisma.song.findUnique({
-    where: {
-      id,
-    },
-  });
-  res.json(result);
+app.get(
+  "/api/v1/songs/:id",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    try {
+      const id: number = parseInt(req.params.id);
+      const result = await prisma.song.findUnique({
+        where: {
+          id,
+        },
+      });
+      res.json(result);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+);
+
+//Crear playlist
+
+app.post("/api/v1/playlist", async (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+
+    const playlist = await prisma.playlist.create({
+      include: {
+        songs: true,
+      },
+      data: {
+        name: data.name,
+        user: { connect: { id: data.user_id } },
+        songs: {
+          create: data.songs,
+        },
+      },
+    });
+
+    res
+      .status(201)
+      .json({ message: "playlist created successfully ", info: playlist });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+//Listar playlists
+app.get("/api/v1/playlist", async (_req: Request, res: Response) => {
+  try {
+    const playlists = await prisma.playlist.findMany({
+      select: {
+        id: true,
+        name: true,
+        user_id: true,
+        songs: true,
+      },
+    });
+    res.json(playlists);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+// app.delete("/api/v1/playlist/delete", async (_req: Request, res: Response) => {
+//   const results = await prisma.playlist.deleteMany();
+
+//   res.json(results);
+// });
+
+//Agregar cancion a un playlist
+
+app.put("/api/v1/playlist/add", async (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+
+    const playlist = await prisma.playlist.update({
+      where: {
+        id: data.id_playlist,
+      },
+      include: {
+        songs: true,
+      },
+      data: {
+        songs: { connect: { id: data.id_song } },
+      },
+    });
+
+    res.json({ message: "song added successfully", info: playlist });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
